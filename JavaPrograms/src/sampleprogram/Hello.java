cf59b70cb31147ad3a20322bf829ebac0b4faae9
Dithya <Dithya@Dithya-PC>
2023-04-10 11:02:15 +0530

    FirstCommit

---
+++ JavaPrograms/src/sampleprogram/Hello.java
@@ -0,0 +1,18 @@
+package sampleprogram;
+
+public class Hello {
+	public void display()
+	{
+		
+		System.out.println("Hello World");
+		 
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Hello obj=new Hello();
+obj.display();
+	}
+
+}
